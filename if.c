da6506c9469227f73ae59a795b4e534e2c09a00b
rian1022 <110169953+rian1022@users.noreply.github.com>
2022-08-05 20:30:08 +0900

    Create if.c

---
+++ if.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+
+int main(void) {
+
+  //정수 입력 후 음수? 양수?
+  // int a; scanf("%d", &a);
+  // if (a < 0) printf("음수");
+  // else printf("양수");
+
+  //정수입력후 홀 짝 구하기
+  // int b; scanf("%d", &b);
+  // if(b % 2 == 0) printf("짝수");
+  // else printf("홀수");
+
+  //정수 입력후 3의 배수인가?
+  // int c; scanf("%d", &c);
+  // if(c % 3 == 0) printf("ㅇ");
+  // else printf("ㄴ");
+
+  //정수두개 큰수가 작은수로 나우어 떨어지는가?
+  // int d, e; scanf("%d %d", &d, &e);
+  // if(d < e){
+  //   if(d % e == 0) printf("ㅇ");
+  //   else printf("ㄴ");
+  // }
+  // else{
+  //   if(e % d == 0) printf("ㅇ");
+  //   else printf("ㄴ");
+  // }
+
+  //실수 절대값
+  // float f; scanf("%f", &f); float g = f * -1;
+  // if(f < 0) printf("%lf", g);
+  // else printf("%lf", f);
+
+  //실수 다섯개 가장 큰 수
+  // float h, i, j, k, l; scanf("%f %f %f %f %f", &h, &i, &j, &k, &l); float big;
+  // if(big < h) big = h;
+  // if(big < i) big = i;
+  // if(big < j) big = j;
+  // if(big < k) big = k;
+  // if(big < l) big = l;
+  // printf("%lf", big);
+
+  
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  // 좌석수 별 차종
+  // int a; scanf("%d", &a);
+  // if(a == 2) printf("스포츠카");
+  // if(a == 2) printf("승용차");
+  // if(a == 2) printf("SUV");
+  // if(a == 2) printf("승합차");
+  // if(a == 2) printf("미니버스");
+  // else printf("ㄴ");
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  //정수 입력후 2와 3의 공배수인지
+  // int a; scanf("%d", &a);
+  // if (a % 2 == 0){
+  //   if (a % 3 == 0) printf("ㅇ");
+  //   else printf("ㄴ");
+  // }
+  // else printf("ㄴ");
+
+  //정수 입력후 12와 30의 공약수인지
+  // int b; scanf("%d", &b);
+  // if (12 % b == 0){
+  //   if (30 % b == 0) printf("ㅇ");
+  //   else printf("ㄴ");
+  // }
+  // else printf("ㄴ");
+
+  //세 정수중 가장 큰값과 작은값
+  // int c, d, e; scanf("%d %d %d", &c, &d, &e);
+  // int big; int small;
+  // if (big < c) big = c;
+  // if (big < d) big = d;
+  // if (big < e) big = e;
+  // if (small > c) small = c;
+  // if (small > d) small = d;
+  // if (small > e) small = e;
+  // printf("%d %d", big, small);
+
+  // 실수 반올림
+  // float f; float g; scanf("%f %f", &f, &g);
+  // if (g < 0.5) printf("%lf", f - g);
+  // else printf("%lf", 1 + f - g);
+
+  // 세 실수 내림차순
+  // float h, i, j; scanf("%f %f %f", &h, &i, &j); float s;
+  // if(s > h) s = h;
+  // if(s > i) s = i;
+  // if(s > j) s = j;
+  // if(s == h){
+  //   if(i < j) printf("%lf %lf %lf", h, i, j);
+  //   else printf("%lf %lf %lf", h, j, i);
+  // }
+  // if(s == i){
+  //   if(h < j) printf("%lf %lf %lf", i, h, j);
+  //   else printf("%lf %lf %lf", i, j, h);
+  // }
+  // if(s == j){
+  //   if(h < i) printf("%lf %lf %lf", j, h, i);
+  //   else printf("%lf %lf %lf", j, i,h);
+  // }
+
+
+  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  
+  return 0;
+}
