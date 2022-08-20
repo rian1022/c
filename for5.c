34e8f5c27cd0381567d6d2dfd6e00ef1cd04bea6
rian1022 <110169953+rian1022@users.noreply.github.com>
2022-08-20 14:23:36 +0900

    Create for5.c

---
+++ for5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  int i;
+  for(i = 1; i < 11; i++){
+    // if(i % 2 == 0) printf("1 ");
+    // else printf("-1 ");
+    // if(i % 2 == 0) printf("1 ");
+    // else printf("0 ");
+    // if(i % 2 == 0) printf("%d ", i * -1);
+    // else printf("%d ", i);
+  }
+  return 0;
+}
